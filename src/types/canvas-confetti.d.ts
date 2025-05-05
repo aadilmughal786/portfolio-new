f15cfa43174b3f9a077736f6eb5312dbe2cf3802
Aadil Mugal <64457787+aadilmughal786@users.noreply.github.com>
2025-05-05 09:58:43 +0000

    feat : update workflows

---
+++ src/types/canvas-confetti.d.ts
@@ -0,0 +1,11 @@
+declare module 'canvas-confetti' {
+  const confetti: (options?: {
+    particleCount?: number;
+    angle?: number;
+    spread?: number;
+    origin?: { x?: number; y?: number };
+    colors?: string[];
+  }) => void;
+
+  export default confetti;
+}
