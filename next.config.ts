97baba689f770cf41d5115c83e9a97794bb712e6
Aadil Mugal <aadilshe786@gmail.com>
2025-05-05 08:34:21 +0530

    fix : conflict

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/portfolio-new',
 };
 
 export default nextConfig;
