c2dc98c71ec6943db585d570c4bd1a5c9cb338d5
Aadil Mugal <aadilshe786@gmail.com>
2025-05-24 22:21:46 +0530

    feat

--- app/data/home/Testimonials.ts
+++ app/data/home/Testimonials.ts
@@ -3,18 +3,18 @@ import { Testimonial } from '@/types/home/Testimonials.types';
 export const testimonialsData: Testimonial[] = [
   {
     id: 1,
-    name: 'Rohan Raj',
-    role: 'Associate Software Engineer',
-    company: 'Tech Mahindra',
-    image: '/portfolio-new/images/home/testimonials/Rohan.png',
-    text: 'Collaborating with Aadil on our college project was an exceptional experience. Aadil’s ability to simplify complex challenges and craft clean, efficient code brought our web app to life. Aadil’s creativity and precision delivered a seamless, user-friendly result that surpassed expectations.',
-  },
-  {
-    id: 2,
     name: 'Abhiram Kantipudi',
     role: 'Software Engineer',
     company: 'TVS Credit Services Ltd.',
     image: '/portfolio-new/images/home/testimonials/Abhiram.png',
-    text: 'Working alongside Aadil was a standout experience. Aadil’s talent for tackling tough technical challenges with clear, effective solutions was impressive. Aadil’s dedication to high-quality code and user-focused design consistently elevated our projects, delivering remarkable outcomes.',
+    text: 'I got to know Aadil during our full stack internship, and he quickly stood out as someone with a creative mindset. He’s always full of ideas, especially when it comes to front-end development, and he has a great eye for detail. On top of that, Aadil is helpful and approachable—always ready to lend a hand when someone’s stuck. It was genuinely great working alongside him.',
+  },
+  {
+    id: 2,
+    name: 'Rohan Raj',
+    role: 'Associate Software Engineer',
+    company: 'Tech Mahindra',
+    image: '/portfolio-new/images/home/testimonials/Rohan.png',
+    text: 'Working with Aadil on our college project was honestly amazing. He has this knack for breaking down tricky problems and writing super clean, efficient code that really made our web app come together. His creativity and attention to detail gave us a smooth, user-friendly app that turned out even better than we’d hoped.',
   },
 ];
